ae7a4264af8b4bb36b4b9ee56b48d2311c55dc3c
pilicorso <pilarcorso@gmail.com>
2023-06-02 13:19:16 -0300

    getters y setters de cPaciente y enums

--- G30-Corso-Quiroga-TP_Final/cPaciente.cpp
+++ G30-Corso-Quiroga-TP_Final/cPaciente.cpp
@@ -5,3 +5,50 @@
 using namespace std;
 
 #include "cPaciente.h"
+
+cPaciente::cPaciente(string nombre, string sexo, int telefono, eTipoSangre tipoSangre, float salud, int cantTumor)
+{
+	this->nombre = nombre;
+	this->sexo = sexo;
+	this->telefono = telefono;
+	this->tipoSangre = tipoSangre;
+	this->salud = salud;
+	this->cantTumor = cantTumor;
+
+}
+
+void cPaciente::setnombre(string nombre)
+{
+	this->nombre = nombre;
+	return;
+}
+
+void cPaciente::setsexo(string sexo)
+{
+	this->sexo = sexo;
+	return;
+}
+
+void cPaciente::settelefono(int telefono)
+{
+	this->telefono = telefono;
+	return;
+}
+
+void cPaciente::settiposangre(eTipoSangre tipoSangre)
+{
+	this->tipoSangre = tipoSangre;
+	return;
+}
+
+void cPaciente::setsalud(float salud)
+{
+	this->salud = salud;
+	return;
+}
+
+void cPaciente::setcanttumor(int cantTumor)
+{
+	this->cantTumor = cantTumor;
+	return;
+}
--- G30-Corso-Quiroga-TP_Final/cPaciente.h
+++ G30-Corso-Quiroga-TP_Final/cPaciente.h
@@ -16,5 +16,23 @@ private:
 	eTipoSangre tipoSangre;
 	float salud;
 	int cantTumor;
+public:
+	cPaciente(string nombre, string sexo, int telefono, eTipoSangre tipoSangre, float salud, int cantTumor);
+	~cPaciente();
+	string getnombre() { return this->nombre; }
+	void setnombre(string nombre);
+	string getsexo() { return this->sexo; }
+	void setsexo(string sexo);
+	int gettelefono() { return this->telefono; }
+	void settelefono(int telefono);
+	eTipoSangre gettiposangre() { return this->tipoSangre; }
+	void settiposangre(eTipoSangre tipoSangre);
+	float getsalud() { return this->salud; }
+	void setsalud(float salud);
+	int getcanttumor() { return this->cantTumor; }
+	void setcanttumor(int cantTumor);
+
+
+
 };
 
