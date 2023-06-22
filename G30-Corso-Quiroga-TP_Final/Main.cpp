f95d09a8c3841c71940a1374d96027cf15d5a89d
pilicorso <pilarcorso@gmail.com>
2023-06-22 20:45:06 -0300

    MAIN

--- G30-Corso-Quiroga-TP_Final/Main.cpp
+++ G30-Corso-Quiroga-TP_Final/Main.cpp
@@ -16,30 +16,49 @@
 using namespace std;
 
 int main() {
-	cOncologo::tumores = new cLista<cTumor>(4);
-	//en buscartumor asignar un random y leer de la lista
-	cTumor* tumor1 = new cTumor(mediano, cabeza, 40); //por parametro
-	cTumor* tumor2 = new cTumor(*tumor1); //por copia
-	cTumor* tumor3 = new cTumor(); //por defecto
-	cTumor* tumor4 = new cTumor(grande, cuello, 20);
-	cOncologo::tumores->Insertar(tumor1); //estoy insertando un tumor en la lista 
-	cOncologo::tumores->Insertar(tumor3);
-	cOncologo::tumores->Insertar(tumor4);
+	try {
+		cOncologo::tumores = new cLista<cTumor>(4);
+		cTumor* tumor1 = new cTumor(mediano, cabeza, 40); //por parametro
+		cTumor* tumor2 = new cTumor(*tumor1); //por copia
+		cTumor* tumor3 = new cTumor(); //por defecto
+		cTumor* tumor4 = new cTumor(grande, cuello, 20);
 
-	cPaciente* paciente1 = new cPaciente("Pepe", 12345678, "Masculino", 123455689765, O, 30.00, 12/11/20);
-	cCentro* centro = new cCentro();
-	cDosimetrista* dosimetrista = new cDosimetrista();
-	cOncologo* oncologo1 = new cOncologo("Jose Maria", 001);
+		cOncologo::tumores->Insertar(tumor1); //estoy insertando un tumor en la lista 
+		cOncologo::tumores->Insertar(tumor3);
+		cOncologo::tumores->Insertar(tumor4);
 
-	centro->agregarPaciente(paciente1);
-	centro->agregarOncologo(oncologo1);
-	centro->agregarDosimetrista(dosimetrista); 
-	centro->crearFicha(paciente1);
+		cPaciente* paciente1 = new cPaciente("Pepe", 12345678, "Masculino", 123455689765, O, 30.00, 12 / 11 / 20);
+		cCentro* centro = new cCentro();
+		cDosimetrista* dosimetrista = new cDosimetrista();
+		cOncologo* oncologo1 = new cOncologo("Jose Maria", 001);
 
-	centro->tratarPaciente(paciente1);
-	//centro->buscarPacienteTyC();
-	
-	delete centro;
+		cBraquiterapia* braquiterapia = new cBraquiterapia();
+		cRadioterapiaHaz* radioterapiahaz = new cRadioterapiaHaz();
+		cRadioterapiaSistemica* radioterapiasistemica = new cRadioterapiaSistemica();
 
-	return 0;
+		centro->agregarPaciente(paciente1);
+		centro->agregarOncologo(oncologo1);
+		centro->agregarDosimetrista(dosimetrista);
+		centro->crearFicha(paciente1);
+
+		cout << "Estoy creando una ficha del paciente" << endl;
+
+		centro->tratarPaciente(paciente1);
+
+		cout << "Trato al paciente con su respectiva terapia" << endl;
+
+		centro->buscarPacienteTyC(braquiterapia, cabeza);
+		centro->buscarPacienteTyC(radioterapiahaz, cuello);
+		centro->buscarPacienteTyC(radioterapiasistemica, tiroides);
+
+		centro->listarPacientes();
+
+		delete centro;
+	}
+	catch (exception& e) {
+		cout << "Error" << e.what() << endl;
+		return 0;
+	}
+		return 0;
+
 }
